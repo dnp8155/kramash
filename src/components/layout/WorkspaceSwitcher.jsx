53d7f3eb855b647d9a3dbaa07e258e6d7f5b28a8
ns50 <ns50@dipakpatel.site>
2026-08-29 14:47:08 +0000

    साइडबार लोगो का आकार बढ़ाकर 44px करें

--- src/components/layout/WorkspaceSwitcher.jsx
+++ src/components/layout/WorkspaceSwitcher.jsx
@@ -34,7 +34,7 @@ export default function WorkspaceSwitcher({ mobile = false, collapsed = false, o
     <div className="px-3 py-3">
       <div className="flex items-center gap-2.5">
         {/* Avatar */}
-        <Logo size={36} />
+        <Logo size={44} />
 
         {/* Text */}
         <div className="flex-1 min-w-0">
