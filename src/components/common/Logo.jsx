204dc80ebb77ffdd6f43c75e22516b07e18e08be
ns50 <ns50@dipakpatel.site>
2026-08-29 14:45:17 +0000

    Update Logo component to render with black rounded box and object-contain

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -3,11 +3,16 @@ export const LOGO_URL =
 
 export default function Logo({ size = 36, className = "" }) {
   return (
-    <img
-      src={LOGO_URL}
-      alt="Kramashah"
+    <div
       style={{ width: size, height: size }}
-      className={`rounded-lg object-cover shrink-0 ${className}`}
-    />
+      className={`rounded-lg bg-black overflow-hidden shrink-0 flex items-center justify-center ${className}`}
+    >
+      <img
+        src={LOGO_URL}
+        alt="Kramashah"
+        className="w-full h-full object-contain"
+        draggable={false}
+      />
+    </div>
   );
 }
