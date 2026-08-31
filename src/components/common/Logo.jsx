153f17157fdbaadd0a94a85130273826d9640c9f
ns50 <ns50@dipakpatel.site>
2026-08-31 16:37:23 +0000

    Revert to previous logo

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -1,5 +1,5 @@
 export const LOGO_URL =
-  "https://media.base44.com/images/public/6a8c4677eeb41482e947f9c6/10810d46e_ChatGPTImageAug31202609_44_54PM.png";
+  "https://media.base44.com/images/public/6a8c4677eeb41482e947f9c6/99658306b_ChatGPTImageAug29202608_10_43PM.png";
 
 export default function Logo({ size = 36, className = "" }) {
   return (
